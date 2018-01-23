88e008181126a291447f964b33b287bf5f1704d5
Shajaroh Chowdhry <shajaroh@gmail.com>
2018-01-23 08:43:32 -0800

    finished sprint

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,12 @@
-class Bee {
-  // TODO..
+
+
+class Bee extends Grub { // look up superclasses methods
+  constructor() {
+    super(Grub)
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
 };
+
+
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,19 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends Bee {
+  constructor() {
+    super(ForagerBee)
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey'; 
+    this.treasureChest = [];
+  }
+
+  gamble (treasure) {
+    this.treasureChest.push(treasure);
+  }  
+
+  forage () {
+    return 'I am too old, let me play cards instead';
+  }
+
 };
